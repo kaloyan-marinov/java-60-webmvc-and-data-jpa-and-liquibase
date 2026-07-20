e3a476c1a10253d19364de721c50891d519321ec
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-07-20 21:42:30 +0200

    initialize this as Spring Boot project by (starting from the parent directory and issuing the following commands): ```shell $ curl https://start.spring.io/starter.zip \     -d type=maven-project \     -d language=java \     -d bootVersion=4.1.0 \     -d groupId=com.github \     -d artifactId=tutorial-about-java-60 \     -d packageName=com.github.tutorial-about-java-60 \     -d packaging=jar \     -d javaVersion=25 \     -d dependencies=web,data-jpa,mysql \     -o tutorial-about-java-60.zip
    $ unzip tutorial-about-java-60.zip \
        -d tutorial-about-java-60
    
    $ rm tutorial-about-java-60.zip
    ```

---
+++ src/main/java/com/github/tutorial_about_java_60/TutorialAboutJava60Application.java
@@ -0,0 +1,13 @@
+package com.github.tutorial_about_java_60;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialAboutJava60Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialAboutJava60Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tutorial_about_java_60/TutorialAboutJava60ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tutorial_about_java_60;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialAboutJava60ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
