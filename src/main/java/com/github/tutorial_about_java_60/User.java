733a307c48ca09cfb92af82af7f2dadc1305b968
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-07-21 21:21:19 +0200

    implement a JPA `@Entity` = `src/main/java/com/github/tutorial_about_java_60/User.java`

---
+++ src/main/java/com/github/tutorial_about_java_60/User.java
@@ -0,0 +1,48 @@
+package com.github.tutorial_about_java_60;
+
+import org.jspecify.annotations.Nullable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/*
+[This annotation] tells Hibernate to make a table out of this class.
+
+Hibernate automatically translates the entity into a table.
+*/
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private @Nullable Integer id;
+
+    private String name;
+
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
