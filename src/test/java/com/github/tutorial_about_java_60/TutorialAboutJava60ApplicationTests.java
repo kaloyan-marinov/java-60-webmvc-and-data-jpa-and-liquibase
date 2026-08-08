b630f9c3098dee5536e0d6f71d3dcd66e8903b0e
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-08-08 09:45:30 +0200

    restoring the test suite to a PASSing state = using the preceding commit to run this repository's test (suite) crashes with ``` Caused by: liquibase.exception.ChangeLogParseException: classpath:/db/changelog/db.changelog-master.yaml does not exist ```

--- src/test/java/com/github/tutorial_about_java_60/TutorialAboutJava60ApplicationTests.java
+++ src/test/java/com/github/tutorial_about_java_60/TutorialAboutJava60ApplicationTests.java
@@ -6,7 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(
 	properties = {
 		"spring.datasource.url=jdbc:h2:mem:test",
-		"spring.jpa.hibernate.ddl-auto=create-drop"
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.liquibase.change-log=classpath:db/changelog/db.changelog-master.mysql.sql"
 	}
 )
 class TutorialAboutJava60ApplicationTests {
