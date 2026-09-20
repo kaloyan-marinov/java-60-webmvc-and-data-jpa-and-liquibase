73e207f8f045dfe31613d150a105bb4f2b5c86ab
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2026-09-20 23:22:58 +0200

    cleanup = improve comments

--- src/main/java/com/github/tutorial_about_java_60/UserRepository.java
+++ src/main/java/com/github/tutorial_about_java_60/UserRepository.java
@@ -2,11 +2,13 @@ package com.github.tutorial_about_java_60;
 
 import org.springframework.data.repository.CrudRepository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// This will be AUTO IMPLEMENTED by Spring into a «bean definition» called `userRepository`
 // CRUD refers Create, Read, Update, Delete
 /*
 Spring automatically implements this repository interface
-in a Bean that has the same name (with a change in the case - it is called `userRepository`).
+in a «bean definition» that has almost the same name as the class
+(the only difference will be a change in the case).
+In this case, the name will of the «bean definition» be `userRepository`.
 */
 public interface UserRepository extends CrudRepository<User, Integer> {
     
